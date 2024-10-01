37071366ce02c5fb1bdbc145a9eb9105326de4ff
Alp Emre Elmas <62440706+alpemreelmas@users.noreply.github.com>
2024-10-01 15:00:30 +0200

    feat: cache and optimize has been added

--- app/Filament/Resources/MenuItemResource/Pages/CreateMenuItem.php
+++ app/Filament/Resources/MenuItemResource/Pages/CreateMenuItem.php
@@ -4,8 +4,16 @@ namespace App\Filament\Resources\MenuItemResource\Pages;
 
 use App\Core\Filament\CoreCreateRecord;
 use App\Filament\Resources\MenuItemResource;
+use App\Models\MenuItem;
+use Illuminate\Support\Facades\Cache;
 
 class CreateMenuItem extends CoreCreateRecord
 {
     protected static string $resource = MenuItemResource::class;
+
+    public function afterCreate()
+    {
+        Cache::set('menu_items',  MenuItem::with('category')->get()->groupBy('category.name'));
+    }
+
 }
